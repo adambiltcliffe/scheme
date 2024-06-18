73e74181beacd2dfb822384b684cd18bc95ba18a
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2024-06-18 21:54:08 +0100

    expression output

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{
 
 type Cell = (Expr, Expr);
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct CellHandle(Weak<Cell>);
 
 impl CellHandle {
@@ -14,7 +14,7 @@ impl CellHandle {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Expr {
     Nil,
     Integer(u64),
@@ -40,37 +40,103 @@ impl PartialEq for Expr {
     }
 }
 
+#[derive(Debug)]
+enum SError {
+    ImproperList,
+}
+
+type SResult<T> = Result<T, SError>;
+
 struct Heap {
-    symbols: Vec<Rc<str>>,
-    cells: Vec<Rc<Expr>>,
+    symbols: Expr,
+    cells: Vec<Rc<(Expr, Expr)>>,
 }
 
 impl Heap {
     fn new() -> Self {
         Self {
-            symbols: Vec::new(),
+            symbols: Expr::Nil,
             cells: Vec::new(),
         }
     }
-}
 
-fn make_symbol(name: &str, heap: &mut Heap) -> Expr {
-    match heap.symbols.iter().find(|s| Rc::deref(*s) == name) {
-        Some(s) => Expr::Symbol(Rc::clone(s)),
-        None => {
-            let new_symbol: Rc<str> = Rc::from(name);
-            heap.symbols.push(Rc::clone(&new_symbol));
-            Expr::Symbol(new_symbol)
+    fn make_cons(&mut self, first: Expr, rest: Expr) -> SResult<Expr> {
+        let cell = Rc::new((first, rest));
+        let handle = CellHandle(Rc::downgrade(&cell));
+        self.cells.push(cell);
+        Ok(Expr::Pair(handle))
+    }
+
+    fn make_symbol(&mut self, name: &str) -> SResult<Expr> {
+        let mut s = self.symbols.clone();
+        while !s.is_nil() {
+            if let Expr::Pair(ch) = s {
+                let pair = ch.get();
+                let (first, rest) = (*pair).clone();
+                if let Expr::Symbol(r) = first {
+                    if Rc::deref(&r) == name {
+                        return Ok(Expr::Symbol(Rc::clone(&r)));
+                    }
+                }
+                s = rest;
+            } else {
+                return Err(SError::ImproperList);
+            }
         }
+        drop(s);
+        let new_symbol: Rc<str> = Rc::from(name);
+        self.symbols =
+            self.make_cons(Expr::Symbol(Rc::clone(&new_symbol)), self.symbols.clone())?;
+        Ok(Expr::Symbol(new_symbol))
+    }
+
+    fn format_expr_inner(&self, expr: &Expr, acc: &mut String) {
+        match expr {
+            Expr::Nil => acc.push_str("nil"),
+            Expr::Integer(n) => acc.push_str(&n.to_string()),
+            Expr::Symbol(s) => acc.push_str(s),
+            Expr::Pair(h) => {
+                acc.push_str("(");
+                let (mut first, mut rest) = (*h.get()).clone();
+                loop {
+                    self.format_expr_inner(&first, acc);
+                    match rest {
+                        Expr::Nil => break,
+                        Expr::Pair(h) => {
+                            acc.push_str(" ");
+                            (first, rest) = (*h.get()).clone()
+                        }
+                        _ => {
+                            acc.push_str(" . ");
+                            self.format_expr_inner(&rest, acc);
+                            break;
+                        }
+                    }
+                }
+                acc.push_str(")");
+            }
+        }
+    }
+
+    fn format_expr(&self, expr: &Expr) -> String {
+        let mut acc = String::new();
+        self.format_expr_inner(expr, &mut acc);
+        acc
     }
 }
 
 fn main() {
     let mut heap = Heap::new();
-    let sym1 = make_symbol("banana", &mut heap);
-    let sym2 = make_symbol("apple", &mut heap);
-    let sym3 = make_symbol("apple", &mut heap);
+    let sym1 = heap.make_symbol("banana").unwrap();
+    let sym2 = heap.make_symbol("apple").unwrap();
+    let sym5 = heap.make_symbol("orange").unwrap();
+    let sym3 = heap.make_symbol("apple").unwrap();
     println!("{:?} {:?} {:?}", sym1, sym2, sym3);
     let sym4 = Expr::Symbol(Rc::from("apple"));
-    println!("{} {}", &sym2 == &sym3, &sym2 == &sym4);
+    println!("{} {} -- {}", sym2 == sym3, sym2 == sym4, heap.cells.len());
+    let data3 = heap.make_cons(Expr::Integer(3), Expr::Nil).unwrap();
+    let data1 = heap.make_cons(Expr::Integer(4), data3).unwrap();
+    let data2 = heap.make_cons(Expr::Integer(5), data1).unwrap();
+    println!("{}", heap.format_expr(&data2));
+    println!("{}", heap.format_expr(&heap.symbols));
 }
