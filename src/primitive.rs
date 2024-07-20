bcca59f32df72ce057c7eb6ba36c224706d4bba9
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2024-07-20 08:40:02 +0100

    added numeric comparison primitives

--- src/primitive.rs
+++ src/primitive.rs
@@ -81,6 +81,34 @@ fn do_divide(args: &Expr, heap: &mut Heap) -> SResult<Expr> {
     do_arithmetic(args, heap, 1, |a, b| a / b)
 }
 
+fn do_predicate(args: &Expr, heap: &mut Heap, pred: impl Fn(i64, i64) -> bool) -> SResult<Expr> {
+    validate_arg_count(heap, args, 2)?;
+    let arg1 = as_integer(&heap.get_first(args)?)?;
+    let arg2 = as_integer(&heap.get_first(&heap.get_rest(args)?)?)?;
+    let result = pred(arg1, arg2);
+    Ok(Expr::Boolean(result))
+}
+
+fn do_numeq(args: &Expr, heap: &mut Heap) -> SResult<Expr> {
+    do_predicate(args, heap, |a, b| a == b)
+}
+
+fn do_lt(args: &Expr, heap: &mut Heap) -> SResult<Expr> {
+    do_predicate(args, heap, |a, b| a < b)
+}
+
+fn do_lte(args: &Expr, heap: &mut Heap) -> SResult<Expr> {
+    do_predicate(args, heap, |a, b| a <= b)
+}
+
+fn do_gt(args: &Expr, heap: &mut Heap) -> SResult<Expr> {
+    do_predicate(args, heap, |a, b| a > b)
+}
+
+fn do_gte(args: &Expr, heap: &mut Heap) -> SResult<Expr> {
+    do_predicate(args, heap, |a, b| a >= b)
+}
+
 fn add_primitive(heap: &mut Heap, name: &str, func: Native) -> SResult<()> {
     let sym = heap.make_symbol(name)?;
     let env = heap.root_env.clone();
@@ -104,5 +132,10 @@ pub(crate) fn add_primitives(heap: &mut Heap) -> SResult<()> {
     add_primitive(heap, "-", do_minus)?;
     add_primitive(heap, "*", do_times)?;
     add_primitive(heap, "/", do_divide)?;
+    add_primitive(heap, "=", do_numeq)?;
+    add_primitive(heap, "<", do_lt)?;
+    add_primitive(heap, "<=", do_lte)?;
+    add_primitive(heap, ">", do_gt)?;
+    add_primitive(heap, ">=", do_gte)?;
     Ok(())
 }
